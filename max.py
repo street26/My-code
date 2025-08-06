ce1be07a527cc9c5259acebf9c9952f1a2c0be53
street painter <sapratejh001@gmail.com>
2025-08-06 10:50:40 +0530

    max.py

---
+++ max.py
@@ -0,0 +1,8 @@
+def Find_max(a,b,c):
+    return max(a,b,c)
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+
+print("The Max Value is: ",max(a,b,c))
